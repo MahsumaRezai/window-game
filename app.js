a37fd9cbda4d7af1295f87d8fce7f6023a601ab8
Your Name <you@example.com>
2023-03-01 14:13:12 -0800

    ex

---
+++ app.js
@@ -0,0 +1,36 @@
+/*function back() {
+    let red = Math.round(Math.random() * 1000) % 255;
+    let bule = Math.round(Math.random() * 1000) % 255;
+    let green = Math.round(Math.random() * 1000) % 255;
+
+    document.getElementById("body").style.backgroundColor = "rgb(" + red + "," + bule + "," + green + ")"
+
+
+}
+window.setInterval(back, 1000)
+
+*/
+
+// object 
+
+
+let obj = {};
+obj["name"] = "Masoumeh";
+obj["age"] = 22;
+obj["university"] = "AFG";
+
+let obj2 = obj;
+
+
+if (obj === obj2) {
+    console.log("Hello form the js code")
+}
+
+
+
+
+
+
+
+
+
